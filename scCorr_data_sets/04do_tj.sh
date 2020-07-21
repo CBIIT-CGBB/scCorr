fa7791390874a3a65f89f228a32ac9a28244b7bf
Ying Hu <11201085+CBIIT-CGR@users.noreply.github.com>
2020-07-21 11:41:45 -0400

    Add files via upload

---
+++ scCorr_data_sets/04do_tj.sh
@@ -0,0 +1,16 @@
+./04do_tj.R NKCells 1
+./04do_tj.R CD4Tcells 1
+./04do_tj.R CD14Monocytes 1
+./04do_tj.R Bcell 1
+./04do_tj.R IL7CD8Tcells 1
+./04do_tj.R Dendriticcell 1
+./04do_tj.R FCGR3AMonocytes 1
+./04do_tj.R CD8Tcells 1
+./04do_tj.R NKCells 2
+./04do_tj.R CD4Tcells 2
+./04do_tj.R CD14Monocytes 2
+./04do_tj.R Bcell 2
+./04do_tj.R IL7CD8Tcells 2
+./04do_tj.R Dendriticcell 2
+./04do_tj.R FCGR3AMonocytes 2
+./04do_tj.R CD8Tcells 2
