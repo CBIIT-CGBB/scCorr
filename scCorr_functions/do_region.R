d004b227aed0ded15901d4f16e9d3b1c1e250e78
Ying Hu <11201085+CBIIT-CGR@users.noreply.github.com>
2020-07-22 08:28:58 -0400

    Add files via upload

--- scCorr_functions/do_region.R
+++ scCorr_functions/do_region.R
@@ -5,31 +5,46 @@
 ## m=w: split a vector to l groups (length.out) by width
 ## m=d: split a vector to l groups (length.out) by density
 
-do_region <- function(clu, clu.c, l=100, m="width"){
+do_region <- function(clu, clu.c, l=100, m="w"){
   if (m=="w"){
     po    <- seq(min(clu[,clu.c]), max(clu[,clu.c]), length.out = l+1);
+    from  <- po[1];
+    out.s <- list();
+    k     <- 0;
+    for (i in 2:length(po)){
+      k <- k + 1;
+      to <- po[i];
+      if (k==1){
+        j  <- which(clu[,clu.c] >= from & clu[,clu.c] <= to);
+      } else {
+        j  <- which(clu[,clu.c] > from & clu[,clu.c] <= to);
+      }
+      if (length(j) < 1){
+        out.s[[k]] <- NA;
+        next;
+      }
+      out.s[[k]] <- row.names(clu)[j];
+      from <- to;
+    }
   } else if (m=="d"){
     po    <- seq(1, nrow(clu), length.out = l+1);
-    
-  }
-
-  from  <- po[1];
-  out.s <- list();
-  k     <- 0;
-  for (i in 2:length(po)){
-    k <- k + 1;
-    to <- po[i];
-    if (k==1){
-      j  <- which(clu[,clu.c] >= from & clu[,clu.c] <= to);
-    } else {
-      j  <- which(clu[,clu.c] > from & clu[,clu.c] <= to);
+    po    <- round(po);
+    o.i   <- order(clu[,clu.c]);
+    o.n   <- row.names(clu)[o.i];
+    from  <- po[1];
+    k     <- 0;
+    out.s <- list();
+    for (i in 2:length(po)){
+      k <- k + 1;
+      to <- po[i];
+      if (k > 1){
+        from <- from - 1;
+      }
+      out.s[[k]] <- o.n[from:to];
+      from <- to;
     }
-    if (length(j) < 1){
-      out.s[[k]] <- NA;
-      next;
-    }
-    out.s[[k]] <- row.names(clu)[j];
-    from <- to;
+  } else {
+    stop("m==\"w\" or m==\"d\"");
   }
   ## return list
   return(region=out.s)
