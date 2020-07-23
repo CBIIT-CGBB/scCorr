4ec54af468d4d88edc859eff47dd5663726ec444
Heng Xu <Heng19@users.noreply.github.com>
2020-07-23 19:36:56 -0400

    Update Test.R

--- scCorr_functions/Test.R
+++ scCorr_functions/Test.R
@@ -1,3 +1,5 @@
+source('../scCorr_functions/scCorr.R')
+
 ct   <- "Bcell";
 infd <- paste0("../scCorr_data_sets/ct_", ct, ".RData")
 load(infd)
@@ -5,6 +7,6 @@ load(infd)
 gene1 <- 'ENSG00000035681'
 gene2 <- 'ENSG00000000419'
 
-genes = t(as.matrix(c(gene1,gene2)))
+target_genes = t(as.matrix(c(gene1,gene2)))
 
-scCorr(cdat,'cluster',genes, num_cluster= 30,perplex = 30)
+scCorr(dat = cdat,method ='cluster',genes = target_genes, num_cluster= 30,perplex = 30)
