df6ce4c46967c2867c9a3815efc422f70807566b
Heng Xu <Heng19@users.noreply.github.com>
2020-07-23 19:41:48 -0400

    Update Test.R

--- scCorr_functions/Test.R
+++ scCorr_functions/Test.R
@@ -10,3 +10,4 @@ gene2 <- 'ENSG00000000419'
 target_genes = t(as.matrix(c(gene1,gene2)))
 
 scCorr(dat = cdat,method ='cluster',genes = target_genes, num_cluster= 30,perplex = 30)
+# method options: 'window', 'trajectory','cluster'
