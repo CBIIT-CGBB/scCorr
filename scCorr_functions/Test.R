554d209c6bdecc9ba7d58f42016c7cf65370a39d
Heng Xu <Heng19@users.noreply.github.com>
2020-07-27 18:27:21 -0400

    Correct tj file and update test file

--- scCorr_functions/Test.R
+++ scCorr_functions/Test.R
@@ -1,12 +1,27 @@
-source('~/Desktop/P/Test/R/scCorr.R')
-
+source('~/R/scCorr.R')
+library(dplyr)
+library(sqldf)
+library(ggplot2)
 ct   <- "Bcell";
-infd <- paste0("../scCorr_data_sets/ct_", ct, ".RData")
+infd <- paste0("~/scCorr_data_sets/ct_", ct, ".RData")
 load(infd)
+gene_info <- read.table('~/scCorr_data_sets/hsa04024_f.txt')
+gene_encoding <-read.table('~/scCorr_data_sets/gene4matrix.txt',header = TRUE)
 
-gene1 <- 'ENSG00000035681'
-gene2 <- 'ENSG00000000419'
+df3 <- sqldf("SELECT gene2, gene as Gene1
+            FROM gene_info
+            Join gene_encoding
+on (gene_info.gene1 = gene_encoding.sym)")
 
-target_genes = t(as.matrix(c(gene1,gene2)))
+df4 <- sqldf("SELECT Gene1, gene as Gene2
+            FROM df3
+            Join gene_encoding
+on (df3.gene2 = gene_encoding.sym)")
 
-scCorr(dat = cdat,method ='window',window_method = 'd',genes = target_genes, num_windows = 50, perplex = 30,max_iteration = 1000)
+
+result1 <- scCorr(cdat = cdat,method ='window',window_method = 'd',genes = df4, num_windows = 50, perplex = 30,max_iteration = 1000)
+result2 <- scCorr(cdat = cdat,method ='window',window_method = 'w',genes = df4, num_windows = 50, perplex = 30,max_iteration = 1000)
+result3 <- scCorr(cdat = cdat,method ='trajectory',genes = df4, perplex = 30,max_iteration = 1000)
+result4 <- scCorr(cdat = cdat,method ='cluster', num_cluster =50,genes = df4, perplex = 30,max_iteration = 1000)
+
+
--- scCorr_functions/cor_tj.R
+++ scCorr_functions/cor_tj.R
@@ -2,19 +2,28 @@ cor_tj <- function(dat_tsne,dat,genes){
   out.s <- NULL
   c1.i <- which.min(dat_tsne[,1])
   c2.i <- which.max(dat_tsne[,1])
-  out  <- tjGCluster2(dat_tsne, from=c1.i, to=c2.i, cutoff=10000);
+  out  <- tjGCluster2(dat_tsne, from=c1.i, to=c2.i,cutoff = 1000)
+  node <- out$node
+  level <- out$level
   for (i in 1:nrow(genes)){
+  #  print(i)
     gene1 <- genes[i,1]
     gene2 <- genes[i,2]
-    ov1   <- do_tj(out$node, out$level1, dat, gene1);
-    ov2   <- do_tj(out$node, out$level1, dat, gene2);
+    print(c(gene1,gene2))
+    ov1   <- do_tj(node, level, dat, gene1);
+
+    ov2   <- do_tj(node, level, dat, gene2);
     n1.i  <- which(ov1$n==1);
     n2.i  <- which(ov2$n==1);
     n0.i  <- unique(c(n1.i, n2.i));
     v1    <- ov1$v[-n0.i];
     v2    <- ov2$v[-n0.i];
-    out   <- summary(glm(v1~ v2))
-    out.s <- rbind(out.s, c(gene1, gene2,out$coef[2,3], out$coef[2,4]))
+    if(var(v1) == 0 | var(v2) == 0){
+      print('not found')
+      next
+    }
+    res <- cor.test(v1, v2, method = "pearson")
+    out.s <- rbind(out.s, c(gene1, gene2,res$p.value, res$estimate))
   }
   return(out.s)
 }
@@ -25,11 +34,12 @@ do_tj <- function(node, l, dat, gene){
   for (i in 1:length(node)){
     j <- grep(node[i], l);
     n <- NULL;
-    for (jj in i){
+    for (jj in j){
       n <- c(n, l[[jj]])
     }
-    ind.i <- which(colnames(dat) %in% n);
-    v     <- as.numeric(dat[gene, ind.i]);
+    n = as.numeric(n)
+  #  print(n)
+    v     <- as.numeric(dat[gene, n]);
     out.v <- c(out.v, mean(v));
     out.n <- c(out.n, length(v));
   }
