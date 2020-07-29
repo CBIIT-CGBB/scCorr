8ea4e8fabe51821ce69f73f0397a9d2c7d0e4fef
Heng Xu <Heng19@users.noreply.github.com>
2020-07-29 16:01:48 -0400

    Correct the errors

--- scCorr_functions/Test.R
+++ scCorr_functions/Test.R
@@ -1,27 +1,24 @@
-source('~/R/scCorr.R')
-library(dplyr)
-library(sqldf)
-library(ggplot2)
+source('../R/scCorr.R')
+source('../R/m_list.R')
+source('../R/get_value.R')
 ct   <- "Bcell";
-infd <- paste0("~/scCorr_data_sets/ct_", ct, ".RData")
+infd <- paste0("~/Desktop/P/scCorr/scCorr_data_sets/ct_", ct, ".RData")
 load(infd)
-gene_info <- read.table('~/scCorr_data_sets/hsa04024_f.txt')
-gene_encoding <-read.table('~/scCorr_data_sets/gene4matrix.txt',header = TRUE)
-
-df3 <- sqldf("SELECT gene2, gene as Gene1
-            FROM gene_info
-            Join gene_encoding
-on (gene_info.gene1 = gene_encoding.sym)")
-
-df4 <- sqldf("SELECT Gene1, gene as Gene2
-            FROM df3
-            Join gene_encoding
-on (df3.gene2 = gene_encoding.sym)")
 
 
-result1 <- scCorr(cdat = cdat,method ='window',window_method = 'd',genes = df4, num_windows = 50, perplex = 30,max_iteration = 1000)
-result2 <- scCorr(cdat = cdat,method ='window',window_method = 'w',genes = df4, num_windows = 50, perplex = 30,max_iteration = 1000)
-result3 <- scCorr(cdat = cdat,method ='trajectory',genes = df4, perplex = 30,max_iteration = 1000)
-result4 <- scCorr(cdat = cdat,method ='cluster', num_cluster =50,genes = df4, perplex = 30,max_iteration = 1000)
+# Dimension Reduction Fisrt: T-sne or Umap
+library(Rtsne)
+dat <- Rtsne(t(dat), dims = 2, perplexity= 30, verbose=TRUE, PCA = TRUE, max_iter = 1000)
+dat <- tsne$Y
+
+# Get the sliced window index list, 
+# Input: one dimensional vector and required number of windows.
 
 
+
+m.list <- m_list(dat[,1],30)
+
+
+# Compute the corresponding vector
+
+v <- get_value(cdat,m.list)
--- scCorr_functions/get_value.R
+++ scCorr_functions/get_value.R
@@ -1,12 +1,12 @@
 get_value <- function(dat,w_list){
+  num_window <- length(w_list)
   res = matrix(, nrow = dim(dat)[1], ncol = num_window)
   for(i in 1:nrow(dat)){
     values <- rep(0,num_window)
     for( j in 1:num_window){
-      values[j] <- mean(as.numeric(dat[i,target_indices[[j]]]))
+      values[j] <- mean(as.numeric(dat[i,w_list[[j]]]))
     }
     res[i,] <- values
-    print(i)
   }
   
   return(res)
