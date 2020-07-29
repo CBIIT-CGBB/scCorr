0a17656a7bf7bfbba91146c754b5b244615b12ed
Heng Xu <Heng19@users.noreply.github.com>
2020-07-29 11:36:33 -0400

    create cluster method list

---
+++ scCorr_functions/c_list.R
@@ -0,0 +1,11 @@
+c_list <- function(dat_dr,l){
+  out.s <- NULL
+  num_clu = l
+  g_clut <- GCluster(dat_dr, k = num_clu,method =  'fast_greedy')
+  g_label <- g_clut$membership
+  target_indices <- vector(mode = "list", length = num_clu)
+  for (i in 1:num_clu){
+    target_indices[i] <- list(which(g_label == i))
+  }
+  return(target_indices)
+}
