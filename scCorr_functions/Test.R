4fd1a533a16e8b63db8799721d6574d4a09df4c1
Heng Xu <Heng19@users.noreply.github.com>
2020-07-31 13:48:08 -0400

    Add files via upload

--- scCorr_functions/Test.R
+++ scCorr_functions/Test.R
@@ -9,19 +9,35 @@ load(infd)
 
 
 # Dimension Reduction Fisrt: T-sne or Umap
+
 library(Rtsne)
 dat <- Rtsne(t(cdat), dims = 2, perplexity= 30, verbose=TRUE, PCA = TRUE, max_iter = 1000)
-dat <- tsne$Y
-row.names(dat) <- colnames(cdat)
+dat_dr <- dat$Y
+# Umap
+#library(umap)
+#dat.umap <- umap(t(cdat))
+#dat_dr <- dat.umap$layout
+
+row.names(dat_dr) <- colnames(cdat)
+
+
+
 
 # Get the sliced window index list, 
 # Input: one dimensional vector and required number of windows.
 
 
 
-m.list <- c_list(dat,30)
-
+m.list <- m_list(dat_dr[,1],100)
+merged.list <- merge_list(m.list)
 
 # Compute the corresponding vector
 
-v <- get_value(cdat,m.list)
+v <- get_value(cdat,merged.list)
+
+# Test Correlation
+v1 = v[2,]
+v2 = v[14272,]
+test = cor.test(v1, v2, method=c("pearson", "kendall", "spearman"))
+test
+
--- scCorr_functions/get_value.R
+++ scCorr_functions/get_value.R
@@ -1,5 +1,9 @@
 get_value <- function(dat,w_list){
   num_window <- length(w_list)
-  res <- sapply(1:num_window,function(x) rowMeans(dat[,w_list[[i]],drop=FALSE]))
+  res = matrix(, nrow = dim(dat)[1], ncol = num_window)
+  for(i in 1:num_window){
+      res[,i] <-rowMeans(dat[,w_list[[i]],drop=FALSE])
+  }
+  row.names(res) <- row.names(dat)
   return(res)
 }
