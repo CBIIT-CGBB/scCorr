027fd432b99e6801b0e621470b08f1b61f9fe089
Heng Xu <Heng19@users.noreply.github.com>
2020-07-31 13:00:09 -0400

    Update get_value.R

--- scCorr_functions/get_value.R
+++ scCorr_functions/get_value.R
@@ -1,5 +1,5 @@
 get_value <- function(dat,w_list){
   num_window <- length(w_list)
-  res <- sapply(1:num_window,function(x) rowMeans(cdat[,w_list[[i]],drop=FALSE]))
+  res <- sapply(1:num_window,function(x) rowMeans(dat[,w_list[[i]],drop=FALSE]))
   return(res)
 }
