34c12b5f30159bc5a1309ac16b5d9536099f1aa0
Heng Xu <Heng19@users.noreply.github.com>
2020-08-09 22:59:21 -0400

    Add files via upload

--- scCorr_functions/m_list.R
+++ scCorr_functions/m_list.R
@@ -1,12 +1,11 @@
 m_list<- function(coordinates,l){
+  cell_name <- names(coordinates)
   num_window <- l
   v_min <- min(coordinates)
   v_max <- max(coordinates)
   v_diff <- (v_max - v_min)/(num_window-1)
   index_list <- as.integer((coordinates - v_min)/v_diff + 1)
-  target_indices <- vector(mode = "list", length = num_window)
-  for (i in 1:num_window){
-    target_indices[i] <- list(which(index_list == i))
-  }
-  return(target_indices)
+  target_indices <- sapply(c(1:num_window),function(x) list(which(index_list == x)))
+  cell <- sapply(c(1:num_window),function(x) list(cell_name[target_indices[[x]]]))
+  return(list(target_indices,cell))
 }
