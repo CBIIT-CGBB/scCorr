5645bfad3520f491788bf28fcbc164df15c3b248
Heng Xu <Heng19@users.noreply.github.com>
2020-08-21 15:03:44 -0400

    Add files via upload

--- scCorr/R/c_list.R
+++ scCorr/R/c_list.R
@@ -3,8 +3,10 @@ c_list <- function(dat_dr = dat_dr,l = 30){
   out.s <- NULL
   num_clu = l
   g_clut <- GCluster::GCluster(dat_dr, k = num_clu,method =  'fast_greedy')
+  g_label <- g_clut$membership
   target_indices <-sapply(c(1:num_clu),function(x) list(which(g_label == x)))
-  cell <- sapply(c(1:num_window),function(x) list(cell_name[target_indices[[x]]]))
+  cell <- sapply(c(1:num_clu),function(x) list(cell_name[target_indices[[x]]]))
   return(list(target_indices,cell))
 }
 
+g_clut$names
