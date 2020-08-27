878b45de4786594fec35529b3534e44a07b2c9fb
Heng Xu <Heng19@users.noreply.github.com>
2020-08-27 13:44:35 -0400

    Update c_list.R

--- scCorr/R/c_list.R
+++ scCorr/R/c_list.R
@@ -2,7 +2,7 @@ c_list <- function(dat = dat_dr,l = 30,method = 'fast_greedy'){
   cell_name <- rownames(dat_dr)
   out.s <- NULL
   num_clu = l
-  g_clut <- GCluster::GCluster(dat_dr, k = num_clu,method =  method)
+  g_clut <- GCluster(dat_dr, k = num_clu,method =  method)
   g_label <- g_clut$membership
   target_indices <-sapply(c(1:num_clu),function(x) list(which(g_label == x)))
   cell <- sapply(c(1:num_clu),function(x) list(cell_name[target_indices[[x]]]))
