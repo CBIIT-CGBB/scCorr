8985ed644bedda70e9d6388361ab05bda5681d23
Heng Xu <Heng19@users.noreply.github.com>
2020-08-29 22:50:26 -0400

    Update toy_data.R

--- scCorr/R/toy_data.R
+++ scCorr/R/toy_data.R
@@ -1,1 +1,10 @@
+#' @examples
+#' library('Rtsne')
+#' data(toy_dat)
+#' dat_dr <- Rtsne(t(toy_dat),pca = TRUE,verbose = TRUE)
+#' dat_dr <- dat_dr$Y
+#' rownames(dat_dr) <- colnames(toy_dat)
+#' c.list <- c_list(dat_dr,20)
+#' new_data <- get_value(dat = toy_dat, d_list = c.list[[1]])
+
 "toy_dat"
