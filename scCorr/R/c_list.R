f3bbee1802d783b2750d004349f5faf833ea4dde
Heng Xu <Heng19@users.noreply.github.com>
2020-12-10 14:30:08 -0500

    Update c_list.R

--- scCorr/R/c_list.R
+++ scCorr/R/c_list.R
@@ -1,12 +1,14 @@
-c_list <- function(dat = dat_dr,l = 30,method = 'fast_greedy'){
+c_list <- function(dat = dat_dr,l = 30,method = 'fast_greedy',auto_scaling = TRUE){
   cell_name <- rownames(dat)
   out.s <- NULL
   num_clu = l
+  if (auto_scaling == TRUE){
   cell.number <- length(dat_dr)
   v <- 112.65840 + 0.01799 * cell.number
-  c1    <- scale.v(dat_dr[,1], -v, v);
-  c2    <- scale.v(dat_dr[,2], -v, v);
-  dat2  <- data.frame(v1=c1, v2=c2);
+  c1    <- scale.v(dat_dr[,1], -v, v)
+  c2    <- scale.v(dat_dr[,2], -v, v)
+  dat  <- data.frame(v1=c1, v2=c2)
+   }
   g_clut <- GCluster(dat2, k = num_clu,method =  method)
   g_label <- g_clut$membership
   target_indices <-sapply(c(1:num_clu),function(x) list(which(g_label == x)))
