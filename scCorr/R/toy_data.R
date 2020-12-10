406a509164984e56f62c9738b495961c1457fbc9
Heng Xu <Heng19@users.noreply.github.com>
2020-12-10 13:50:55 -0500

    Update toy_data.R

--- scCorr/R/toy_data.R
+++ scCorr/R/toy_data.R
@@ -11,6 +11,7 @@
 #' dat_dr <- dat_dr$Y
 #' rownames(dat_dr) <- colnames(toy_dat)
 #' c.list <- c_list(dat_dr,20)
-#' new_data <- get_value(dat = toy_dat, d_list = c.list[[1]])
+#' merged.list <- c.list[[1]]
+#' new_data <- get_value(dat = toy_dat, d_list = merged.list)
 
 "toy_dat"
