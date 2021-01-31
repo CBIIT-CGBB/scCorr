c30c1cf48fe996cee604d066db00d4bcdb4c4db5
Ying Hu <11201085+CBIIT-CGBB@users.noreply.github.com>
2021-01-31 12:20:05 -0500

    Delete get_value.R

--- scCorr_functions/get_value.R
+++
@@ -1,9 +0,0 @@
-get_value <- function(dat,w_list){
-  num_window <- length(w_list)
-  res = matrix(, nrow = dim(dat)[1], ncol = num_window)
-  for(i in 1:num_window){
-      res[,i] <-rowMeans(dat[,w_list[[i]],drop=FALSE])
-  }
-  row.names(res) <- row.names(dat)
-  return(res)
-}
