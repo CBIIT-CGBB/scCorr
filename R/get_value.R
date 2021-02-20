1bc50ea46e811c2f1383b1678d81a59e92418f5f
Heng Xu <Heng19@users.noreply.github.com>
2021-02-20 15:21:42 -0500

    Update get_value.R

--- R/get_value.R
+++ R/get_value.R
@@ -1,10 +1,18 @@
 get_value <- function(dat = dat,d_list = d_list){
-  num_window <- length(d_list)
-  res = matrix(, nrow = dim(dat)[1], ncol = num_window)
+  num_c <- length(d_list)
+  res = matrix(, nrow = dim(dat)[1], ncol = num_c)
+  if is.list(d_list){
   for(i in 1:num_window){
       res[,i] <- rowMeans(dat[,d_list[[i]],drop=FALSE])
   }
   row.names(res) <- row.names(dat)
+  }
+  else {
+    for(i in 1:num_c){
+      clu.u.i = d_list[i]
+      c.j <- which(clu.f==clu.u.i);
+      res[,i] <- rowMeans(dat[,c.j,drop=FALSE])
+      }
+  }
   return(res)
 }
-
