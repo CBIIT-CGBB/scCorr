353f66e2ccbf2d172545a84afec72c9bdb0d4346
Ying Hu <11201085+CBIIT-CGBB@users.noreply.github.com>
2021-03-03 12:31:35 -0500

    Update tj_list.R

--- R/tj_list.R
+++ R/tj_list.R
@@ -1,8 +1,8 @@
-tj_list <- function(dat = dat_dr, ctf = 100){
+tj_list <- function(dat = dat, ctf = 100){
   out.s <- NULL
-  c1.i <- which.min(dat_dr[,1])
-  c2.i <- which.max(dat_dr[,1])
-  out  <- tjGCluster2(dat_dr,from=c1.i, to=c2.i,cutoff = ctf)
+  c1.i <- which.min(dat[,1])
+  c2.i <- which.max(dat[,1])
+  out  <- tjGCluster2(dat, from=c1.i, to=c2.i,cutoff = ctf)
   node <- out$node
   level <- out$level1
   cell <- level[sapply(level, function(x) length(x) >= 2)]
