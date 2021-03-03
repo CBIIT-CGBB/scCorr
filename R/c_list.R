b674b11c462a8e3a26a75392ba1f2b3cde10d6c2
Ying Hu <11201085+CBIIT-CGBB@users.noreply.github.com>
2021-03-03 08:33:06 -0500

    Update c_list.R

--- R/c_list.R
+++ R/c_list.R
@@ -1,12 +1,12 @@
-c_list <- function(dat = dat_dr,l = 30,method = 'fast_greedy',auto_scaling = TRUE){
+c_list <- function(dat = dat,l = 30,method = 'fast_greedy',auto_scaling = TRUE){
   cell_name <- rownames(dat)
   out.s <- NULL
   num_clu = l
   if (auto_scaling == TRUE){
-  cell.number <- length(dat_dr)
+  cell.number <- length(dat)
   v <- 112.65840 + 0.01799 * cell.number
-  c1    <- scale.v(dat_dr[,1], -v, v)
-  c2    <- scale.v(dat_dr[,2], -v, v)
+  c1    <- scale.v(dat[,1], -v, v)
+  c2    <- scale.v(dat[,2], -v, v)
   dat  <- data.frame(v1=c1, v2=c2)
    }
   g_clut <- GCluster(dat, k = num_clu,method =  method)
