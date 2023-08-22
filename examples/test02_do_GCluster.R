ef03c53daee1b09a33a5fd342841e129a149c542
Ying Hu <11201085+CBIIT-CGBB@users.noreply.github.com>
2023-08-22 10:45:22 -0400

    Update test02_do_GCluster.R

--- examples/test02_do_GCluster.R
+++ examples/test02_do_GCluster.R
@@ -12,7 +12,7 @@ dat  <- read.table("../data/01/do_tsne30_2000.txt");
 k     <- 50;
 ## scale region is from v1 to v2
 v2    <- 300;
-v1    <- -1 * s.v2;
+v1    <- -1 * v2;
 c1    <- scale.v(dat[,1], v1, v2);
 c2    <- scale.v(dat[,2], v1, v2);
 dat2  <- data.frame(v1=c1, v2=c2);
