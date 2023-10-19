7622d88590a7f414b0187fd0fb164e28ba795790
Ying Hu <11201085+CBIIT-CGBB@users.noreply.github.com>
2023-10-19 16:57:15 -0400

    Update merge_cluster.R

--- R/merge_cluster.R
+++ R/merge_cluster.R
@@ -1,3 +1,14 @@
+dist2df <- function(inDist) {
+  if (class(inDist) != "dist") stop("wrong input type")
+  A <- attr(inDist, "Size")
+  B <- if (is.null(attr(inDist, "Labels"))) sequence(A) else attr(inDist, "Labels")
+  if (isTRUE(attr(inDist, "Diag"))) attr(inDist, "Diag") <- FALSE
+  if (isTRUE(attr(inDist, "Upper"))) attr(inDist, "Upper") <- FALSE
+  data.frame(
+    row = B[unlist(lapply(sequence(A)[-1], function(x) x:A))],
+    col = rep(B[-length(B)], (length(B)-1):1),
+    value = as.vector(inDist))
+}
 
 merge_cluster <- function(dat, cutoff=5){
   clu.t   <- table(dat[,3]);
