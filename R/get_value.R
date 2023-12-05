07204000f6d5ef39c940c477c94191f1b3e08a2a
Ying Hu <yhu@mail.nih.gov>
2023-12-05 08:24:53 -0500

    Update get_value.R

--- R/get_value.R
+++ R/get_value.R
@@ -5,6 +5,7 @@ get_value <- function(dat = dat, d_list = d_list){
     for(i in 1:num_c){
       res[,i] <- rowMeans(dat[,d_list[[i]],drop=FALSE])
     }
+    colnames(res) <- names(d_list)
   } else {
     ## unique cluster ID
     clu.u <- sort(unique(d_list));
@@ -15,6 +16,7 @@ get_value <- function(dat = dat, d_list = d_list){
       c.j     <- which(d_list==clu.u.i);
       res[,i] <- apply(dat[, c.j], 1, function(x) mean(x, na.rm = T))
     }
+    colnames(res) <- clu.u                   
   }
   row.names(res) <- row.names(dat)
   return(res)
